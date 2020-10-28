610d2bbd7e484525aee139b3ce0be7281e589559
cyb3rl3x <alexlimacavalera@gmail.com>
2020-10-28 11:16:45 -0300

    aula 01

--- desafios/nlw_project/web/backend/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import {OrphanagesController} from './Controllers/OrphanagesController'
-
-const routes = Router();
-
-routes.post('/orphanages', async(request, response)=>{})
-
-
-export default routes;
